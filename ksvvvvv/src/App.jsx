5a756c4057865fc74596b07fd85b726525bfb84e
DevK5103 <kansaradev95@gmail.com>
2026-03-26 14:26:26 +0530

    Initial university website structure with routing and pages

---
+++ ksvvvvv/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import Admissions from './pages/Admissions';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div style={{ padding: '2rem' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/admissions" element={<Admissions />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+}
+
+export default App
